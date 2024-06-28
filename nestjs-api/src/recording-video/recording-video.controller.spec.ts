73626129be1fa27d748185d9ed9b22e30f040ac2
b4nksw <bu.kswong@gmail.com>
2024-06-28 15:31:53 -0700

    test(recording-video): add tests for recording video controller and service

--- nestjs-api/src/recording-video/recording-video.controller.spec.ts
+++ nestjs-api/src/recording-video/recording-video.controller.spec.ts
@@ -1,18 +1,68 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { RecordingVideoController } from './recording-video.controller';
+import { RecordingVideoService } from './recording-video.service';
 
 describe('RecordingVideoController', () => {
   let controller: RecordingVideoController;
+  let service: RecordingVideoService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [RecordingVideoController],
+      providers: [RecordingVideoService],
     }).compile();
 
     controller = module.get<RecordingVideoController>(RecordingVideoController);
+    service = module.get<RecordingVideoService>(RecordingVideoService);
+  });
+
+  afterEach(() => {
+    jest.restoreAllMocks();
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('should return processed recording video data', () => {
+    // mock data
+    const mockRecordingVideoData = {
+      isRecordingStarted: false,
+      isStartRecordingPending: false,
+      isStopRecordingPending: false,
+      videos: [
+        {
+          id: 'mockId-0',
+          url: 'mockUrl-0',
+          price: -5,
+          endAt: '1719565175850',
+        },
+        {
+          id: 'mockId-1',
+          url: 'mockUrl-1',
+          price: 0,
+          endAt: '1719565174850',
+        },
+      ],
+    };
+
+    // spies
+    const fetchRecordingVideoDataSpy = jest
+      .spyOn(service, 'fetchRecordingVideoData')
+      .mockReturnValue(mockRecordingVideoData);
+
+    // call the actual method
+    const result = controller.getProcessedVideos();
+
+    // assertions
+    expect(fetchRecordingVideoDataSpy).toHaveBeenCalledTimes(1);
+
+    expect(result.isRecordingStarted).toBe(false);
+    expect(result.isStartRecordingPending).toBe(false);
+    expect(result.isStopRecordingPending).toBe(false);
+
+    const firstVideo = result.videos[0];
+    expect(firstVideo.id).toBe('mockId-1');
+    expect(firstVideo.url).toBe('mockUrl-1');
   });
 });
--- nestjs-api/src/recording-video/recording-video.service.spec.ts
+++ nestjs-api/src/recording-video/recording-video.service.spec.ts
@@ -12,7 +12,76 @@ describe('RecordingVideoService', () => {
     service = module.get<RecordingVideoService>(RecordingVideoService);
   });
 
+  afterEach(() => {
+    jest.restoreAllMocks();
+  });
+
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should return a recording video data', () => {
+    // call the actual method
+    const result = service.fetchRecordingVideoData();
+
+    // assertions
+    expect(result).toHaveProperty('isRecordingStarted');
+    expect(result).toHaveProperty('isStartRecordingPending');
+    expect(result).toHaveProperty('isStopRecordingPending');
+    expect(result).toHaveProperty('videos');
+  });
+
+  it('should return processed videos', () => {
+    // mock data
+    const mockRecordingVideoData = {
+      isRecordingStarted: false,
+      isStartRecordingPending: false,
+      isStopRecordingPending: false,
+      videos: [
+        {
+          id: 'mockId-0',
+          url: 'mockUrl-0',
+          price: -5,
+          endAt: '1719565175850',
+        },
+        {
+          id: 'mockId-1',
+          url: 'mockUrl-1',
+          price: 5,
+          endAt: '1719565174850',
+        },
+      ],
+    };
+
+    // call the actual method
+    const result = service.processRecordingVideo(mockRecordingVideoData);
+
+    // assertions
+    const firstVideo = result[0];
+    expect(firstVideo.id).toBe('mockId-1');
+    expect(firstVideo.url).toBe(null);
+  });
+
+  it('should return empty videos', () => {
+    // mock data
+    const mockRecordingVideoData = {
+      isRecordingStarted: true,
+      isStartRecordingPending: true,
+      isStopRecordingPending: true,
+      videos: [
+        {
+          id: 'mockId-0',
+          url: 'mockUrl-0',
+          price: -5,
+          endAt: '1719565175850',
+        },
+      ],
+    };
+
+    // call the actual method
+    const result = service.processRecordingVideo(mockRecordingVideoData);
+
+    // assertions
+    expect(result.length).toBe(0);
   });
 });
