3d562e23cac5008c1060a535ab4d074308f612bd
b4nksw <bu.kswong@gmail.com>
2024-06-30 00:20:06 -0700

    fix(nextjs-component): change class to className

--- nextjs-component/pages/recording-videos.js
+++ nextjs-component/pages/recording-videos.js
@@ -1,17 +1,17 @@
 const RecordingVideos = ({ videos }) => {
   return (
-    <div class='mx-4'>
-      <div class='text-lg font-semibold p-2 border-b-2'>Recording Videos</div>
+    <div className='mx-4'>
+      <div className='text-lg font-semibold p-2 border-b-2'>Recording Videos</div>
       {videos.length === 0 ? (
-        <div class='p-3'>No videos available...</div>
+        <div className='p-3'>No videos available...</div>
       ): (
         <div>
           {videos.map((video) => (
-            <div key={video.id} class='p-2 border-b-2'>
-              <p><span class='font-medium'>ID:</span> {video.id}</p>
-              {video.price <= 0 && <p><span class='font-medium'>URL</span>: {video.url}</p>}
-              <p><span class='font-medium'>Price:</span> {video.price}</p>
-              <p><span class='font-medium'>End At:</span> {video.endAt}</p>
+            <div key={video.id} className='p-2 border-b-2'>
+              <p><span className='font-medium'>ID:</span> {video.id}</p>
+              {video.price <= 0 && <p><span className='font-medium'>URL</span>: {video.url}</p>}
+              <p><span className='font-medium'>Price:</span> {video.price}</p>
+              <p><span className='font-medium'>End At:</span> {video.endAt}</p>
             </div>
           ))}
         </div>
